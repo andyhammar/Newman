6ad3df4613c90a80608899ca85c9d84148cd913c
Andreas Hammar <andreas.hammar@jayway.com>
2013-04-29 00:51:07 +0200

    logos and color

--- src/Newman.Domain/GeoService.cs
+++ src/Newman.Domain/GeoService.cs
@@ -1,14 +1,21 @@
-﻿using MightyLittleGeodesyPcl.Positions;
+﻿using System.Globalization;
+using MightyLittleGeodesyPcl.Positions;
 using Newman.Domain.ViewModels;
 
 namespace Newman.Domain
 {
     public class GeoService
     {
+        static GeoService()
+        {
+            _cultureInfo = new CultureInfo("sv-se");
+        }
+        private static readonly CultureInfo _cultureInfo;
+
         public Position GetPosition(string x, string y)
         {
-            var y1 = double.Parse(y.Replace(',','.'));
-            var x1 = double.Parse(x.Replace(',', '.'));
+            var y1 = double.Parse(y, _cultureInfo);
+            var x1 = double.Parse(x, _cultureInfo);
             var swePos = new SWEREF99Position(y1, x1, SWEREF99Position.SWEREFProjection.sweref_99_13_30);
             var wgsPos = swePos.ToWGS84();
 
