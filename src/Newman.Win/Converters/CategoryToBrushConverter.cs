4ba4659a853be0362a08b4db53f6b2b3f53d29c7
Andreas Hammar <andreas.hammar@jayway.com>
2013-04-29 00:31:09 +0200

    colors for category

---
+++ src/Newman.Win/Converters/CategoryToBrushConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Newman.Win.Converters
+{
+    public class CategoryToBrushConverter : IValueConverter
+    {
+        static CategoryToBrushConverter()
+        {
+            CategoryToBrushDict = new Dictionary<string, SolidColorBrush>
+                {
+                    {"Nedlägges", 
+                        Colors.Gray.ToBrush()},
+                    {"Gym", 
+                        Colors.GhostWhite.ToBrush()},
+                    {"Närlekplats", 
+                        Colors.DodgerBlue.ToBrush()},
+                    {"Kvarterslekplats", 
+                        Colors.YellowGreen.ToBrush()},
+                    {"Temalekplats", 
+                        Colors.OrangeRed.ToBrush()},
+                    {"Stadsdelslekplats", 
+                        Colors.DarkRed.ToBrush()},
+                    {"Områdeslekplats", 
+                        Colors.ForestGreen.ToBrush()},
+                };
+            DefaultBrush = Colors.Black.ToBrush();
+        }
+
+        public static Dictionary<string, SolidColorBrush> CategoryToBrushDict;
+        public static SolidColorBrush DefaultBrush;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var text = value as string;
+            if (text != null && CategoryToBrushDict.ContainsKey(text))
+                return CategoryToBrushDict[text];
+            return DefaultBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Newman.Win/Converters/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace Newman.Win.Converters
+{
+    public static class Extensions
+    {
+        public static SolidColorBrush ToBrush(this Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+    }
+}
