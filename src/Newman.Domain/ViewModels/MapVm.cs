16a5a6526c4a90e4c2ddf8cfec5bc9927047cfc9
Andreas Hammar <andreas.hammar@jayway.com>
2013-04-28 23:33:21 +0200

    showing map

---
+++ src/Newman.Domain/ViewModels/MapVm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Newman.Domain.ViewModels
+{
+    public class MapVm
+    {
+        public MapVm()
+        {
+            Playgrounds = new DataRepo().GetPlaygrounds();
+        }
+
+        public IEnumerable<PlaygroundIvm> Playgrounds { get; set; } 
+    }
+}
