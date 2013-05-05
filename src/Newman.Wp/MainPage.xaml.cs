ee4768415532b9d589937a7f34230ea6d98ae37a
Andreas Hammar <andreas.hammar@jayway.com>
2013-05-05 23:21:09 +0200

    showing pushpings

--- src/Newman.Wp/MainPage.xaml.cs
+++ src/Newman.Wp/MainPage.xaml.cs
@@ -1,24 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Device.Location;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Controls.Maps.Platform;
+using Newman.Domain.ViewModels;
 
 namespace Newman.Wp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private MapVm _vm;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            _vm = new MapVm();
+            DataContext = _vm;
+            _vm.PropertyChanged += _vm_PropertyChanged;
+        }
+
+        void _vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Center")
+            {
+                var convert = new PositionToLocationConverter().Convert(_vm.Center, null, null, null);
+                map.Center = convert as GeoCoordinate;
+            }
+        }
+
+        protected async override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            await _vm.Init();
         }
     }
 }
---
+++ src/Newman.Wp/PositionToLocationConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Newman.Domain.ViewModels;
+
+namespace Newman.Wp
+{
+    public class PositionToLocationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var position = value as Position;
+            var result = new System.Device.Location.GeoCoordinate(position.Lat, position.Lng);
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
